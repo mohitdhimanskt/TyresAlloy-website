8b4af642ebab5904e0cac44336a711e312e172dc
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-08-27 13:23:38 +0530

    Add to users

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env("URL", "http://localhost:1337"),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'c872ab3dee85a4c7b215ce35b4958164'),
