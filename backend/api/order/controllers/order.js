5aa9ba738972d42bc0ae424bb6d774b1c23c862c
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-09-02 17:31:16 +0530

    Add to oder

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -1,8 +1,37 @@
 'use strict';
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-
-module.exports = {};
+const stripe = require('stripe')(process.env.STRIPE_KEY)
+const MY_DOMAIN = 'http://localhost:3000/cart';
+module.exports = {
+    async create(ctx) {
+        const { cartDetail, cartTotal } = ctx.request.body
+        //build line items array
+        const line_items = cartDetail.map((cartItem) => {
+            const item = {}
+            item.price_data = {
+                currency: 'usd',
+                product_data: {
+                    name: cartItem.Name,
+                    images: [`${cartItem.url}`]
+                },
+                unit_amount: (cartItem.price * 100).toFixed(0),
+            },
+            item.quantity = cartItem.quantity
+            return item;
+        })
+        // create order
+        await strapi.services.order.create({ Item: line_items })
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items,
+            mode: 'payment',
+            success_url: `${MY_DOMAIN}?success=true`,
+            cancel_url: `${MY_DOMAIN}?canceled=true`,
+        })
+        return { id: session.id}
+    } 
+};
